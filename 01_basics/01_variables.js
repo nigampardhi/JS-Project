9cecd504f7129a0d4cde5123313e903d3a9cd360
Nigam <nigam@amuratech.com>
2025-05-16 10:23:11 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,5 +3,9 @@ let email = "nigam@dev.com"
 var password = "123456789"
 let accountCity;
 
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.log(accountId)
 console.table([accountId, email, password, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => unique
+//object
