009285ff16bb534edad9af90f9737b4ad4ef843e
Nigam <nigam@amuratech.com>
2025-05-21 10:22:13 +0530

    conversion of datatype  part1

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = 33
+let score = "33we"
+console.log(score)
+console.log(typeof score) //number
+
+valueInNumber = Number(score)
+
+console.log(valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+isLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn) //true
+
+
+// 1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
+// null => false
+
+let someNumber = 33
+someString = String(someNumber)
+console.log(someString)//33
+console.log(typeof someString)//string
